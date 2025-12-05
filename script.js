0de59aa9a05ae72b84189f6d902a79555ab2e1c7
kxntaphat <124706579+kxntaphat@users.noreply.github.com>
2025-12-05 23:30:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function playMusic() {
+    document.getElementById("bgm").play();
+}
+
+function showMsg() {
+    document.getElementById("msg").innerText =
+        "คนที่แอบชอบก็คือ… เรานี่แหละ 😳💕";
+}
